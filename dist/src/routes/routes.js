7d563884abc36f70992c0e3c640d0100989d3dc8
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-02-02 16:53:48 -0300

    Update package.json

--- dist/src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const FindAllColorsController_1 = require("../controllers/colors/FindAllColorsController");
-const FindByIdColorController_1 = require("../controllers/colors/FindByIdColorController");
-exports.routes = (0, express_1.Router)();
-exports.routes.get("/api/colors", new FindAllColorsController_1.FindAllColorsController().handle);
-exports.routes.get("/api/color", new FindByIdColorController_1.FindByIdColorController().handle);
